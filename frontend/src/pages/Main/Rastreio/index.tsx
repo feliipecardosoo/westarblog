8959309e2efa3e7288ca2cd79b26b5a18be65512
feliipecardosoo <fglibino@gmail.com>
2024-02-28 15:28:00 -0300

    finalizado 100%

--- frontend/src/pages/Main/Rastreio/index.tsx
+++ frontend/src/pages/Main/Rastreio/index.tsx
@@ -7,10 +7,13 @@ export default function MyComponent() {
     const [response, setResponse] = useState<string>(""); 
 
     const handleButtonClick = async () => {
-        if (inputValue) {
-            const result = await fazerRequisicao(inputValue); 
-            setResponse(result || "Nenhuma resposta recebida."); 
+        if (!inputValue) { 
+            setResponse("Valor inválido."); 
+            return; 
         }
+
+        const result = await fazerRequisicao(inputValue); 
+        setResponse(result || "Nenhuma resposta recebida."); 
     };
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,13 +24,12 @@ export default function MyComponent() {
         <div className={style.container}>
             <h1 className={style.container__h1}> Rastreie sua Mercadoria! </h1>
             <input type="text" placeholder='Digite a Chave de Acesso da CTE' value={inputValue} onChange={handleInputChange} className={style.container__input} />
-            {/* Renderize o botão para acionar a requisição */}
             <button onClick={handleButtonClick} className={style.container__button}>
                 Fazer Requisição
             </button>
-            {/* Renderize a resposta abaixo do botão */}
             <div dangerouslySetInnerHTML={{ __html: response }} className={style.container__resposta}/>
         </div>
     );
 }
+
 
