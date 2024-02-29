89b0d1edd15bd0ffa4f20d3fc911abe6bb18a7dc
feliipecardosoo <fglibino@gmail.com>
2024-02-29 15:00:07 -0300

    Alterando numero no zap

--- frontend/src/pages/Main/Cotacao/Conteudo_Dois/index.tsx
+++ frontend/src/pages/Main/Cotacao/Conteudo_Dois/index.tsx
@@ -8,7 +8,7 @@ export default function Cotacao() {
                 <h1 className={style.h1}>Venha fazer uma Cotaçao conosco!</h1>
                 <p className={style.p}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>   
-                <a className={style.whatsapp} href="whatsapp://send?phone=48999684045&text=Olá,%20gostaria%20de%20falar%20com%20você" target="_blank">
+                <a className={style.whatsapp} href="whatsapp://send?phone=48984480413&text=Olá,%20gostaria%20de%20falar%20com%20você" target="_blank">
                     <img src={logo} alt="Logo wpp" className={style.whatsapp__img} />
                 </a>     
         </div>
